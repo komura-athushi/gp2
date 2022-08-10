a18744b3d7e1dd2d66a6860d93968b6dedbe4322
komura-athushi <kbc18b09@stu.kawahara.ac.jp>
2022-08-11 00:32:20 +0900

    目次追加(修正予定)。

--- source/template.js
+++ source/template.js
@@ -14,3 +14,22 @@ function toggleElement(id)
     }
     ele.classList.add('d-none');
 }
+
+//目次を追加したい要素、目次を検索する親要素
+function displayTableContents(ele,parent)
+{
+    const chileds = parent.childNodes;
+    let string ="";
+    for(let i = 0;i < chileds.length;i++)
+    {
+        const chiled = chileds.item(i);
+        if(chiled.tagName!='LI') continue;
+        const content = document.createElement('a');
+        content.href = '#' + chiled.id;
+        content.innerHTML = chiled.textContent;
+        content.classList.add('margin-left');
+
+        ele.append(content);
+       
+    }
+}
