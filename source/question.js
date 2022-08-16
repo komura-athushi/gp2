9b82590982d4a29c959f4d07a1f53437167a1b52
komura-athushi <kbc18b09@stu.kawahara.ac.jp>
2022-08-17 00:37:17 +0900

    第三章、執筆開始。

--- source/question.js
+++ source/question.js
@@ -10,4 +10,25 @@ function displayAnswer()
     {
         answer.parentNode.classList.add("error-answer");
     }
+    
+    if (window.File) {
+        // File APIに関する処理を記述
+        window.alert("File APIが実装されてます。");
+      } else {
+        window.alert("本ブラウザではFile APIが使えません");
+      }
+
+    /*
+    var fs = new ActiveXObject("Scripting.FileSystemObject");
+    
+    // text.txtという新規のファイルを作成
+    var file = fs.CreateTextFile("text.txt");
+
+    // texxt.txtファイルへ書き込み
+    file.Write("Complete, written!");
+
+    // text.txtファイルを閉じる
+    file.Close();
+
+    */
 }
